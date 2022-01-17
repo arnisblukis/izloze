8928a93a344cd752f1751a518dcd84aa522f3147
arnisblukis <97448396+arnisblukis@users.noreply.github.com>
2022-01-17 15:07:57 +0200

    trešais

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const vardi=['Kristaps Celms','Marta Puķe','Leonīds Āpsis','Laura Bērziņa',
 const balvas=['Dators','Telefons','Automašīna','Ceļojums','Planšetdators'];
 const naudaKopa=1000000;// izlozes kopējā summa
 let uzvaretajuSkaits=5;
-let rand= Math.random()*10;
+let rand= Math.random()*5;
 rand= Math.floor(rand);// noapaļo skaitli uz leju
 console.log(vardi[rand]);//izvada konsolē nejauši izvēlētus vārdus
