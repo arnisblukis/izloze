6df5bc7ec95ae632231896597bea81481097d58c
Arnis Blukis <97448396+arnisblukis@users.noreply.github.com>
2022-04-21 15:36:38 +0300

    izloze1
    izloze

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ let uzvaretajuSkaits=5;
 let rand= Math.random()*5;
 rand= Math.floor(rand);// noapaļo skaitli uz leju
 console.log(vardi[rand]);//izvada konsolē nejauši izvēlētus vārdus
+for (let i=0; i<uzvaretajuSkaits; i++){
+    let rand=Math.random()* vardi.length;
+    rand=Math.floor(rand);// noapaļo uz leju
+
+
+    let uzvaretajs=vardi[rand];// izvada konsolē
+    let laimesti=balvas[rand];
+
+    rindas.innerHTML += `
+    <tr>
+    <td>${i+1}</td>
+    <td>${uzvaretajs}</td>
+    <td>${laimesti}</td>
+    </tr>`
+}
